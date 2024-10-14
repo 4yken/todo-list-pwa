8c643caeff56742d6ad7bd85e0cbffa2c387cb5c
4yken <126425493+4yken@users.noreply.github.com>
2024-10-14 00:33:31 -0600

    ADD:beforeinstallprompt

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,57 @@
-import { useReducer, useRef } from "react";
-import './App.css'
+import { useReducer, useRef, useEffect, useState } from "react";
+import './App.css';
 
 function App() {
-  const inputRef = useRef();
-
+    const inputRef = useRef();
     const [tasks, dispatch] = useReducer((state = [], action) => {
         switch (action.type) {
-            case 'add_task': {
+            case 'add_task':
                 return [
                     ...state,
                     { id: state.length, title: action.title }
-                ]
-            }
-            case 'remove_task': {
-                return state.filter((task, index) => index != action.index);
-            }
-            default: {
+                ];
+            case 'remove_task':
+                return state.filter((task, index) => index !== action.index);
+            default:
                 return state;
-            }
         }
     });
+
+    const [showInstallPrompt, setShowInstallPrompt] = useState(false);
+    const [deferredPrompt, setDeferredPrompt] = useState(null);
+
+    useEffect(() => {
+        // Capturar el evento 'beforeinstallprompt'
+        window.addEventListener('beforeinstallprompt', (e) => {
+            e.preventDefault(); // Prevenir la alerta automática del navegador
+            setDeferredPrompt(e); // Guardar el evento para más tarde
+            setShowInstallPrompt(true); // Mostrar la alerta personalizada
+        });
+    }, []);
+
+    const handleInstallClick = () => {
+        if (deferredPrompt) {
+            deferredPrompt.prompt(); // Mostrar la ventana de instalación
+            deferredPrompt.userChoice.then((choiceResult) => {
+                if (choiceResult.outcome === 'accepted') {
+                    console.log('El usuario aceptó instalar la PWA');
+                } else {
+                    console.log('El usuario rechazó la instalación');
+                }
+                setDeferredPrompt(null); // Limpiar el evento
+                setShowInstallPrompt(false); // Ocultar la alerta
+            });
+        }
+    };
+
+    const handleAlertResponse = (response) => {
+        if (response) {
+            handleInstallClick(); // Si el usuario aceptó, iniciar la instalación
+        } else {
+            console.log('El usuario rechazó la instalación de la PWA.');
+            setShowInstallPrompt(false); // Cerrar la alerta
+        }
+    };
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -28,33 +60,72 @@ function App() {
             title: inputRef.current.value
         });
         inputRef.current.value = '';
-    }
+    };
 
-  return (
-    <>
-    <h1 className="text-4xl font-bold text-zinc-800 mb-6 text-center">Lista de tareas</h1>
-        <form onSubmit={handleSubmit} className="max-w-md mx-auto p-4 bg-white shadow-lg rounded-lg">
-            <div className="mb-4">
-              <label className=" text-zinc-700 text-lg font-semibold mb-3">Tarea</label>
-              <input type="text" name="title" ref={inputRef} className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-lime-400"
-                    placeholder="Escribe una tarea..." />
-            </div>
-            <button type="submit" className="w-full bg-lime-500 text-white px-4 py-2 rounded-md shadow-sm hover:bg-lime-600 focus:outline-none focus:ring-2 focus:ring-lime-400">
-            Enviar
-            </button>
-        </form>
-        <div className="tasks max-w-md mx-auto mt-6">
-            {tasks && tasks.map((task, index) => (
-                <div className="task flex items-center justify-between bg-gray-100 p-4 mb-2 rounded-md shadow-sm" key={index}>
-                    <p className="text-gray-700">{task.title}</p>
-                    <button onClick={() => dispatch({ type: 'remove_task', index })} className="bg-red-500 text-white px-2 py-1 rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400">
-                        Borrar
-                    </button>
+    return (
+        <>
+            <h1 className="text-4xl font-bold text-zinc-800 mb-6 text-center">Lista de tareas</h1>
+            <form onSubmit={handleSubmit} className="max-w-md mx-auto p-4 bg-white shadow-lg rounded-lg">
+                <div className="mb-4">
+                    <label className="text-zinc-700 text-lg font-semibold mb-3">Tarea</label>
+                    <input
+                        type="text"
+                        name="title"
+                        ref={inputRef}
+                        className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-lime-400"
+                        placeholder="Escribe una tarea..."
+                    />
                 </div>
-            ))}
-        </div>
-    </>
-  )
+                <button
+                    type="submit"
+                    className="w-full bg-lime-500 text-white px-4 py-2 rounded-md shadow-sm hover:bg-lime-600 focus:outline-none focus:ring-2 focus:ring-lime-400"
+                >
+                    Enviar
+                </button>
+            </form>
+
+            <div className="tasks max-w-md mx-auto mt-6">
+                {tasks && tasks.map((task, index) => (
+                    <div
+                        className="task flex items-center justify-between bg-gray-100 p-4 mb-2 rounded-md shadow-sm"
+                        key={index}
+                    >
+                        <p className="text-gray-700">{task.title}</p>
+                        <button
+                            onClick={() => dispatch({ type: 'remove_task', index })}
+                            className="bg-red-500 text-white px-2 py-1 rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400"
+                        >
+                            Borrar
+                        </button>
+                    </div>
+                ))}
+            </div>
+
+            {showInstallPrompt && (
+                <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex justify-center items-center">
+                    <div className="alert p-4 bg-white border rounded shadow-lg">
+                        <p>¿Quieres instalar la PWA?</p>
+                        <div className="flex justify-center space-x-4 mt-4">
+                            <button
+                                onClick={() => handleAlertResponse(true)}
+                                className="bg-lime-500 text-white px-4 py-2 rounded-md"
+                            >
+                                Aceptar
+                            </button>
+                            <button
+                                onClick={() => handleAlertResponse(false)}
+                                className="bg-gray-300 text-gray-700 px-4 py-2 rounded-md"
+                            >
+                                Cancelar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+
+        </>
+    );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      manifest:{
-        display: 'standalone',
-        display_override:['window-controls-overlay'],
-        lang:'es-ES',
-        name: 'Lista de Tareas',
-        short_name: 'TO-DO List',
-        description: 'PWA Lista de tareas',
-        theme_color: '#ffffff',
-        background_color:'',
+      manifest: {
+        name: "Lista de Tareas",
+        short_name: "TO-DO List",
+        description: "PWA Lista de tareas",
+        lang: "es-ES",
+        display: "standalone",
+        display_override: ["window-controls-overlay"],
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
         icons: [
           {
-            src: 'Demogorgon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose:'any',
+            src: "/icons/Demogorgon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any"
           },
           {
-            src: 'Demogorgon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose:'maskable',
+            src: "/icons/Demogorgon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable"
           }
         ]
       }
     })
-  ],
-})
+  ]
+});
